982a69252858b613bf039a11c88aaeaebd0e7dee
platonov1727 <platonov1727@yandex.ru>
2023-04-14 16:31:31 +0300

    users Done

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -150,6 +150,6 @@ DJOSER = {
     'PERMISSIONS': {
 
         'user': ['rest_framework.permissions.IsAuthenticated'],
-        'user_list': ['rest_framework.permissions.IsAuthenticated'],
+        'user_list': ['rest_framework.permissions.AllowAny'],
     },
 }
